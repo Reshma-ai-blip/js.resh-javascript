1eb032fb1f7ae62d9dadd2b046399135a702a5d8
Reshma-ai-blip <shaik.resh1994@gmail.com>
2026-07-28 17:30:29 +0000

    jsonapi

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -46,10 +46,33 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+//console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
+//console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+ const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+ }
+
+ // course.courseInstructor
+
+ const {courseInstructor} = course
+
+ //console.log(courseInstructor);
+
+ /*{
+    "name": "Kainaat",
+    "coursename": "js in hindi",
+    "price": "free"
+ }*/
+
+[
+    
+]
